df53a21e932f68fbcbc4b70afb0a8c99b4f05cf7
angeldzz <pintodiazangel@gmail.com>
2026-03-06 13:59:50 +0100

    Add project files.

---
+++ MvcCorePaginacionRegistros/Controllers/PaginacionController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcCorePaginacionRegistros.Models;
+using MvcCorePaginacionRegistros.Repositories;
+
+namespace MvcCorePaginacionRegistros.Controllers
+{
+    public class PaginacionController : Controller
+    {
+        private RepositoryHospital repo;
+        public PaginacionController(RepositoryHospital repo)
+        {
+            this.repo = repo;
+        }
+        public async Task<IActionResult> RegistroVistaDepartamento(int? posicion)
+        {
+            if (posicion == null)
+            {
+                posicion = 1;
+            }
+            int numeroRegistros = await this.repo.GetNumeroRegistrosVistaDepartamentosAsync();
+            //primero 1
+            //ultimo 4
+            //anterior posicion - 1
+            //siguiente posicion + 1
+            int siguiente = posicion.Value + 1;
+            if (siguiente > numeroRegistros)
+            {
+                siguiente = numeroRegistros;
+            }
+            int anterior = posicion.Value - 1;
+            if (anterior < 1)
+            {
+                anterior = 1;
+            }
+            ViewData["ULTIMO"] = numeroRegistros;
+            ViewData["ANTERIOR"] = anterior;
+            ViewData["SIGUIENTE"] = siguiente;
+            VistaDepartamento departamento = await this.repo.GetVistaDepartamentoAsync(posicion.Value);
+            return View(departamento);
+        }
+        public async Task<IActionResult> GrupoVistaDepartamentos(int? posicion)
+        {
+            if (posicion == null)
+            {
+                posicion = 1;
+            }
+            //LO SIGUIENTE SERA QUE DEBEMOS DIBUJAR LOS NUMEROS DE PAGINA EN LOS LINKS
+            //VOY A REALIZAR EL DIBUJO DESDE AQUI
+            int numPagina = 1;
+            int numRegistros = await this.repo.GetNumeroRegistrosVistaDepartamentosAsync();
+            ViewData["ULTIMO"] = numRegistros;
+            ViewData["NUMPAGINA"] = numPagina;
+            
+            
+            List<VistaDepartamento> departamentos = await this.repo.GetGrupoVistaDepartamentosAsync(posicion.Value);
+            return View(departamentos);
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ MvcCorePaginacionRegistros/Repositories/RepositoryHospital.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MvcCorePaginacionRegistros.Data;
+using MvcCorePaginacionRegistros.Models;
+
+namespace MvcCorePaginacionRegistros.Repositories
+{
+    #region VISTAS Y PROCEDIMIENTOS ALMACENADOS
+    /*
+     --ROW_NUMER() OVER (ORDER BY COLUMNA)
+--SELECT TO SELECT
+SELECT * FROM
+(SELECT ROW_NUMBER() OVER (ORDER BY DEPT_NO) AS POSICION,
+DEPT_NO, DNOMBRE, LOC FROM DEPT) QUERY
+WHERE QUERY.POSICION = 2
+
+create VIEW V_DEPARTAMENTO_INDIVIDUAL
+AS
+	SELECT CAST(ROW_NUMBER() OVER (ORDER BY DEPT_NO) as int) AS POSICION,
+	DEPT_NO, DNOMBRE, LOC FROM DEPT
+GO
+
+SELECT * FROM V_DEPARTAMENTO_INDIVIDUAL
+WHERE POSICION = 1
+    */
+    #endregion
+    public class RepositoryHospital
+    {
+        private HospitalContext context;
+        public RepositoryHospital(HospitalContext context)
+        {
+            this.context = context;
+        }
+        public async Task<int> GetNumeroRegistrosVistaDepartamentosAsync()
+        {
+            return await context.VistaDepartamentos.CountAsync();
+        }
+        public async Task<VistaDepartamento> GetVistaDepartamentoAsync(int posicion)
+        {
+            VistaDepartamento departamento =
+                 await this.context.VistaDepartamentos.Where(d => d.Posicion == posicion).FirstOrDefaultAsync();
+            return departamento;
+        }
+        public async Task<List<VistaDepartamento>> GetGrupoVistaDepartamentosAsync(int posicion)
+        {
+            var consulta = from datos in this.context.VistaDepartamentos
+                           where datos.Posicion >= posicion && datos.Posicion < posicion + 2
+                           select datos;
+            return await consulta.ToListAsync();
+        }
+    }
+}
